16a57c1b1f4d730f342dad076b5b88f62d1136cc
Herrera <Isaacoun100@estudiantec.cr>
2021-09-20 01:46:16 -0600

    Main modified for testing
    The main.cpp was moddified with the purpose of testing the code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 #include "logic/example_logic.h"
-o
 #include "gui/example_gui.h"
+#include "socket/SocketServer.h"
+
+SocketServer* communication;
+void * initiateCommunication(void *){
+    try {communication->run();}
+    catch (string ex) {cout<<ex<<endl;}
+}
 
 int main(int argc, char** argv){
-    printa();
-    firtsGUI();
+
+    //printa();
+    //firtsGUI();
+
+    communication = new SocketServer;
+    pthread_t newThread;
+    pthread_create(&newThread, 0 , initiateCommunication, NULL);
+    pthread_detach(newThread);
+
     return 0;
 }
