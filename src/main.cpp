9a205bf4d31442f8c733936399239483ac8bda3e
Carlos12001 <62664256+Carlos12001@users.noreply.github.com>
2021-09-21 14:57:50 -0600

    Add the listenClient with Game class.
    The listenClient was implemented.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,31 @@
 
-
-
 #include "gui/game.h"
+auto game = new Game();
+
+int listenClient(){
+    string txt;
+    cin >> txt;
+    if(txt =="LEFT"){
+        game->moveToLeftBar();
+        cout << " Me MOVI ala izquierda"<< endl;
+        return listenClient();
+    }
+    else if(txt =="RIGHT") {
+        game->moveToRightBar();
+        cout <<" Me MOVI ala derecha"<< endl;
+        return listenClient();
+    }
+    else if (txt=="salir"){
+        game->stop();
+        return 0;
+    }else{
+        return listenClient();
+
+    }
+}
+
 
 int main() {
-    auto game = new Game();
     game->run();
     delete game;
 }
