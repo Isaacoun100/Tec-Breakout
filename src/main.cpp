e884e6d45f046e05ef2b1ad8538f199966e7d2ce
Herrera <Isaacoun100@estudiantec.cr>
2021-09-21 14:30:40 -0600

    Main modified for testing purposes
    Code was added to main to test thee newly added clases

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,60 @@
 #include "logic/example_logic.h"
 #include "gui/example_gui.h"
 #include "socket/SocketServer.h"
+#include "socket/SocketClient.h"
 
-SocketServer* communication;
-void * initiateCommunication(void *){
-    try {communication->run();}
+SocketServer* serverConnection;
+SocketClient* clientConnection;
+
+void * serverInit(void *){
+    try {serverConnection->run();}
+    catch (exception& e) {cerr<<e.what()<<'\n';}
+    return nullptr;
+}
+
+void * clientInit(void *){
+    try {clientConnection->connectServer();}
     catch (exception& e) {cerr<<e.what()<<'\n';}
     return nullptr;
 }
 
 int main(int argc, char** argv){
 
-    //printa();
-    //firtsGUI();
+    /*
 
-    communication = new SocketServer;
-    pthread_t newThread;
-    pthread_create(&newThread, 0 , initiateCommunication, NULL);
-    pthread_detach(newThread);
-
-    string check = "Connected successfully";
+    serverConnection = new SocketServer;
+    pthread_t serverThread;
+    pthread_create(&serverThread, 0 , serverInit, nullptr);
+    pthread_detach(serverThread);
 
     while(true){
         string msg;
         cin >> msg;
         if(msg == "salir") break;
-        communication ->setMessage(msg.c_str());
+        serverConnection ->setMessage(msg.c_str());
+    }
+    delete serverConnection;
+
+    */
+
+    /*
+    clientConnection = new SocketClient;
+    pthread_t clientThread;
+    pthread_create(&clientThread, 0, clientInit, nullptr);
+    pthread_detach(clientThread);
+
+
+    while(true){
+        string msg;
+        cin>>msg;
+        if(msg == "salir") break;
+        clientConnection->setMessage(msg.c_str());
     }
 
-    delete communication;
+    delete clientConnection;
+     */
+
+    
     return 0;
 
 }
