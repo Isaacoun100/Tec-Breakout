f83621bbdc9283c2d5d1b95ef5f54a5753c59e4b
Herrera <Isaacoun100@estudiantec.cr>
2021-09-22 01:56:51 -0600

    Interface added
    This interface will ask for the port and socket

---
+++ src/login/StartClient.cpp
@@ -0,0 +1,60 @@
+#include "StartClient.h"
+#include "../socket/SocketClient.h"
+#include "Login.h"
+
+SocketClient* clientConnection;
+
+int socketPortClient;
+string ipAddress;
+
+void * clientInit(void *){
+
+
+    try {
+        clientConnection->setServerSocket(socketPortClient);
+        clientConnection->setIpAddress(ipAddress);
+        clientConnection->connectServer();
+    }
+    catch (exception& e) {
+        cout<<"An exception was found with the error message\n"<<e.what()<<"\n In line 12 from StartServer.cpp";
+    }
+
+    return nullptr;
+}
+
+StartClient::StartClient() {
+    string option;
+    cout <<"*********************************************************\n"
+           "***    Please enter the ip where the server is in    ****\n"
+           "* if the server is local you can use the word localhost *\n"
+           "*    or enter the value ( 0 ) to return to the login    *\n"
+           "*********************************************************\n";
+    cin >> ipAddress;
+
+    cout <<"*********************************************************\n"
+           "***    Please enter the socket where the server is   ****\n"
+           "***                     is allocated in              ****\n"
+           "*    or enter the value ( 0 ) to return to the login    *\n"
+           "*********************************************************\n";
+    cin >> option;
+
+    if(ipAddress=="0" || option=="0"){
+        Login();
+    }
+
+    stringstream parse(option);
+    parse >> socketPortClient;
+
+    if(socketPortClient<=1024){
+        cout<<"Sorry, your input doesn't match any open ports"<<endl;
+        StartClient();
+    }
+    else{
+        pthread_t serverThread;
+        clientConnection = new SocketClient;
+        pthread_create(&serverThread, nullptr , clientInit, nullptr);
+    }
+
+    delete clientConnection;
+
+}
