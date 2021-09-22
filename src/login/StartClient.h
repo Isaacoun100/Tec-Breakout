605d936d20655d21ba8aaef281ca28bb7586c308
Herrera <Isaacoun100@estudiantec.cr>
2021-09-22 01:57:26 -0600

    Header for the StartClient.cpp

---
+++ src/login/StartClient.h
@@ -0,0 +1,14 @@
+#ifndef TEC_BREAKOUT_STARTCLIENT_H
+#define TEC_BREAKOUT_STARTCLIENT_H
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+class StartClient {
+public:
+    StartClient();
+};
+
+#endif
