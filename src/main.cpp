806d6c258be5f4143db47ef53f2f06a84367789b
Carlos12001 <62664256+Carlos12001@users.noreply.github.com>
2021-09-22 17:49:05 -0600

    Clear code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,4 @@
 
-#include "gui/game.h"
-
-auto game = new Game();
-int listenClient(){
-    string txt;
-    cin >> txt;
-    if(txt =="LEFT"){
-        game->moveToLeftBar();
-        cout << " Me MOVI ala izquierda"<< endl;
-        return listenClient();
-    }
-    else if(txt =="RIGHT") {
-        game->moveToRightBar();
-        cout <<" Me MOVI ala derecha"<< endl;
-        return listenClient();
-    }
-    else if (txt=="salir"){
-        game->stop();
-        return 0;
-    }else{
-        return listenClient();
-
-    }
-}
-
 
 int main() {
-    game->run();
-    delete game;
 }
