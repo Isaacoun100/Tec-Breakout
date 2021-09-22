c66b2ea8484fa9fc4c275f9cd6ffd20b66ba114c
Herrera <Isaacoun100@estudiantec.cr>
2021-09-21 20:46:14 -0600

    Clean main
    The main is now cleaned with no test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,6 @@
-#include "logic/example_logic.h"
-#include "gui/example_gui.h"
-#include "socket/SocketServer.h"
-#include "socket/SocketClient.h"
+#include <iostream>
 
-SocketServer* serverConnection;
-SocketClient* clientConnection;
-
-void * serverInit(void *){
-    try {serverConnection->run();}
-    catch (exception& e) {cerr<<e.what()<<'\n';}
-    return nullptr;
-}
-
-void * clientInit(void *){
-    try {clientConnection->connectServer();}
-    catch (exception& e) {cerr<<e.what()<<'\n';}
-    return nullptr;
-}
-
-int main(int argc, char** argv){
-
-    /*
-
-    serverConnection = new SocketServer;
-    pthread_t serverThread;
-    pthread_create(&serverThread, 0 , serverInit, nullptr);
-    pthread_detach(serverThread);
-
-    while(true){
-        string msg;
-        cin >> msg;
-        if(msg == "salir") break;
-        serverConnection ->setMessage(msg.c_str());
-    }
-    delete serverConnection;
-
-    */
-
-    /*
-    clientConnection = new SocketClient;
-    pthread_t clientThread;
-    pthread_create(&clientThread, 0, clientInit, nullptr);
-    pthread_detach(clientThread);
-
-
-    while(true){
-        string msg;
-        cin>>msg;
-        if(msg == "salir") break;
-        clientConnection->setMessage(msg.c_str());
-    }
-
-    delete clientConnection;
-     */
-
-    
+int main() {
+    std::cout << "Hello, World!" << std::endl;
     return 0;
-
 }
