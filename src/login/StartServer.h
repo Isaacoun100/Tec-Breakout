1a61fb09e61d9514a1427886c3fd25552e79f7ee
Herrera <Isaacoun100@estudiantec.cr>
2021-09-22 01:58:38 -0600

    Header for the StartServer.cpp

---
+++ src/login/StartServer.h
@@ -0,0 +1,14 @@
+#ifndef TEC_BREAKOUT_STARTSERVER_H
+#define TEC_BREAKOUT_STARTSERVER_H
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+class StartServer {
+public:
+    StartServer();
+};
+
+#endif
