517c2094e51fb674273621040c60a2cc0a3e9212
Herrera <Isaacoun100@estudiantec.cr>
2021-09-30 12:22:50 -0600

    Main class commented

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include "login/Login.h"
 #include "temp/ReadWrite.h"
 
+/**
+ * This is the main method that will clear the tmp.txt file and initiate the login interface
+ * @return a 0 value if no issue was found.
+ */
 int main(){
     ReadWrite().clearFile();
     Login();
+    return 0;
 }
