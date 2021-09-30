a9f85339f37f269eebd7dcbd450b7650531b563d
Isaac Herrera Monge <Isaacoun100@gmail.com>
2021-09-29 19:19:24 -0600

    SocketClient Documented

--- src/socket/SocketClient.cpp
+++ src/socket/SocketClient.cpp
@@ -1,18 +1,33 @@
-#include "SocketClient.h"
+ #include "SocketClient.h"
 
-
+/**
+ *This is the constructor for the SocketClient class
+ */
 SocketClient::SocketClient() {}
 
+/**
+ * The setIpAddress method is a setter for the ipAddress variable that contains the ip in
+ * which the connection is going to take place
+ * @param ipAddress is the ip
+ */
 void SocketClient::setIpAddress(string ipAddress){
     this->ipAddress=ipAddress;
     cout<<this->ipAddress;
-
 }
+
+/**
+ * This method is a setter for the serverSocket variable that contains the socket server
+ * where the connection is going to take place.
+ * @param serverSocket is the socket
+ */
 void SocketClient::setServerSocket(int serverSocket) {
     this->serverSocket=serverSocket;
     cout<<this->serverSocket;
 }
 
+/**
+ * This method initialices the connection between the server and the client.
+ */
 void SocketClient::connectServer() {
     identifier = socket(AF_INET, SOCK_STREAM,IPPROTO_TCP);
     if(identifier<0) cout<<"Server was not found"<<endl;
@@ -32,6 +47,12 @@ void SocketClient::connectServer() {
     pthread_create(&clientThread,0,SocketClient::listener, (void*)this);
     pthread_detach(clientThread);
 }
+
+/**
+ * This method listens the messages comming through the server socket to the client.
+ * @param obj
+ * @return
+ */
 void * SocketClient::listener(void *obj) {
     SocketClient* c = (SocketClient*)obj;
 
@@ -55,6 +76,11 @@ void * SocketClient::listener(void *obj) {
     pthread_exit(nullptr);
 }
 
+/**
+ * SetMessage is a method that receives a constant char* that contains the message that
+ * we will sent to the server through the socket and the ip.
+ * @param msn is the message that is going to be sent
+ */
 void SocketClient::setMessage(const char *msn) {
     send(3, msn, strlen(msn), 0);
 }
--- src/socket/SocketClient.h
+++ src/socket/SocketClient.h
@@ -14,15 +14,44 @@
 
 using namespace std;
 
+/**
+ * This class will be used to connect to a server from the client, it will also check open
+ * ports and sent message through the socket
+ */
 class SocketClient {
 public:
-    SocketClient();
-
     int serverSocket;
     string ipAddress;
+
+    /**
+     *This is the constructor for the SocketClient class
+     */
+    SocketClient();
+
+    /**
+     * This method is a setter for the serverSocket variable that contains the socket server
+     * where the connection is going to take place.
+     * @param serverSocket is the socket
+     */
     void setServerSocket(int serverSocket);
+
+    /**
+     * The setIpAddress method is a setter for the ipAddress variable that contains the ip in
+     * which the connection is going to take place
+     * @param ipAddress is the ip
+     */
     void setIpAddress(string ipAddress);
+
+    /**
+     * SetMessage is a method that receives a constant char* that contains the message that
+     * we will sent to the server through the socket and the ip.
+     * @param msn is the message that is going to be sent
+     */
     void setMessage(const char* msn);
+
+    /**
+     * This method initialices the connection between the server and the client.
+     */
     void connectServer();
 
 private:
@@ -30,9 +59,14 @@ private:
     int identifier;
     sockaddr_in info;
 
+    /**
+     * This method listens the messages comming through the server socket to the client.
+     * @param obj
+     * @return
+     */
     static void* listener(void* obj);
 
 };
 
 
-#endif //TEC_BREAKOUT_SOCKETCLIENT_H
+#endif
